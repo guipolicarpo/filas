e51f7832c7280643e2e68626076ee8a326c4c4bb
Guilherme Policarpo <guilhermepc288@gmail.com>
2022-01-10 20:32:05 -0300

    Initial commit

---
+++ src/main/java/dio/No.java
@@ -0,0 +1,38 @@
+package dio;
+
+public class No {
+    private Object object;
+    private No refNo;
+
+    public No(Object object) {
+        this.refNo = null;
+        this.object = object;
+    }
+
+    public No() {
+
+    }
+
+    public No getRefNo() {
+        return refNo;
+    }
+
+    public void setRefNo(No refNo) {
+        this.refNo = refNo;
+    }
+
+    public Object getObject() {
+        return object;
+    }
+
+    public void setObject(Object object) {
+        this.object = object;
+    }
+
+    @Override
+    public String toString() {
+        return "No{" +
+                "object=" + object +
+                '}';
+    }
+}
